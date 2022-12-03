986b69fae705b4d753949692da23f50d723c547c
Maksymilian Zadka <maksymilian.j.zadka@gmail.com>
2022-12-03 21:23:45 +0100

    feat: add logout page

--- apps/employees/src/pages/index.tsx
+++ apps/employees/src/pages/index.tsx
@@ -44,7 +44,9 @@ const AuthShowcase: React.FC = () => {
       {session?.user && <p>{JSON.stringify(session.user)}</p>}
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={session ? () => signOut() : () => signIn()}
+        onClick={session ? () => signOut({
+          callbackUrl: "/logout"
+        }) : () => signIn("auth0")}
       >
         {session ? "Sign out" : "Sign in"}
       </button>
