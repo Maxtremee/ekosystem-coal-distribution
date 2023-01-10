d2c1385eaf2cc4e822dbe7a1c171b1c290f730a1
Maksymilian Zadka <maksymilian.j.zadka@gmail.com>
2023-01-10 22:56:09 +0100

    feat: distribution center details

---
+++ apps/employees/src/pages/distribution-centers/[id].tsx
@@ -0,0 +1,36 @@
+import { Text } from "@ekosystem/ui";
+import { Alert, Spinner } from "flowbite-react";
+import { useRouter } from "next/router";
+import DistributionCenterDetails from "../../modules/DistributionCenters/DistributionCenterDetails";
+import { trpc } from "../../utils/trpc";
+
+export default function DistributionCenterDetailsPage() {
+  const router = useRouter();
+  const id = router.query.id as string;
+
+  const { data, isLoading, error } =
+    trpc.distributionCenters.getDetails.useQuery({
+      id,
+    });
+
+  if (error) {
+    return (
+      <Alert color="failure">
+        {error?.message || "Błąd wczytywania skupu"}
+      </Alert>
+    );
+  }
+
+  if (isLoading) {
+    return <Spinner size="xl" color="success" />;
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Text as="h2" className="text-lg font-semibold">
+        {data[0].name}
+      </Text>
+      <DistributionCenterDetails distributionCenter={data} />
+    </div>
+  );
+}
